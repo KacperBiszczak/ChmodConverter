8115c960eeb496a2e85680806088fc2c8db18f56
Kacper Biszczak <kacper.biszczak@microsoft.wsei.edu.pl>
2024-05-15 15:00:01 +0200

    Implement SymbolicToNumeric method

--- ClassLib/Utils.cs
+++ ClassLib/Utils.cs
@@ -1,10 +1,74 @@
-﻿namespace ClassLib
+﻿using System;
+
+namespace ClassLib
 {
     public static class Utils
     {
         public static string SymbolicToNumeric(string symbolic)
         {
-            throw new NotImplementedException();
+            // Check if argument is null
+            if (ReferenceEquals(symbolic, null))
+            {
+                throw new ArgumentNullException("Argument can't be a null.");
+            }
+
+            symbolic = symbolic.Trim().ToLower();
+
+            // Check format is like "-rwxrwxrwx" 
+            if (symbolic[0] != '-' || symbolic.Length != 10)
+                throw new ArgumentException("Invalid format.");
+
+            string result = "";
+            
+            // Convert symbolic to numeric
+            string[] symbolicPerm = new string[3];
+            for (int i = 0; i < 3; i++)
+            {
+                symbolicPerm[i] = symbolic.Substring(1 + (3 * i), 3);
+                int num;
+
+                switch (symbolicPerm[i])
+                {
+                    case "---":
+                        num = 0;
+                        break;
+
+                    case "--x":
+                        num = 1;
+                        break;
+
+                    case "-w-":
+                        num = 2;
+                        break;
+
+                    case "-wx":
+                        num = 3;
+                        break;
+
+                    case "r--":
+                        num = 4;
+                        break;
+
+                    case "r-x":
+                        num = 5;
+                        break;
+
+                    case "rw-":
+                        num = 6;
+                        break;
+
+                    case "rwx":
+                        num = 7;
+                        break;
+
+                    default:
+                        throw new ArgumentException("Invalid symbols or format.");
+                }
+
+                result += num;
+            }
+
+            return result;
         }
 
         public static string NumericToSymbolic(string numeric)
@@ -33,7 +97,7 @@
                         }
                         else
                         {
-
+                            // Convert numeric to symbolic
                             string symbolicPerm = "";
                             switch (num)
                             {
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine( U.NumericToSymbolic("773") );
+            Console.WriteLine( U.SymbolicToNumeric("-rwxrw-r-x") );
         }
     }
 }
