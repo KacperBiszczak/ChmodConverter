b1d7e38b5243d8a9476fe451f24ab32958218a9c
Kacper Biszczak <kacper.biszczak@microsoft.wsei.edu.pl>
2024-05-22 11:36:24 +0200

    Implement convert method in button

--- ChmodConverter/MainWindow.xaml.cs
+++ ChmodConverter/MainWindow.xaml.cs
@@ -27,14 +27,46 @@ namespace ChmodConverter
 
         private void ButtonConvert_Click(object sender, RoutedEventArgs e)
         {
-            //try
-            //{
-            //    TextBoxSymbolicPerm.Text = Utils.NumericToSymbolic(TextBoxNumericPerm.Text);
-            //}
-            //catch (Exception ex)
-            //{
-            //    Console.WriteLine(ex.Message);
-            //}
+            int[] checkBoxPerms = new int[3];
+
+            // Owner rwx 
+            if (CheckBoxReadOwner.IsChecked == true)
+                checkBoxPerms[0] += 4;
+
+            if (CheckBoxWriteOwner.IsChecked == true)
+                checkBoxPerms[0] += 2;
+
+            if (CheckBoxExecuteOwner.IsChecked == true)
+                checkBoxPerms[0] += 1;
+
+            // Guest rwx
+            if (CheckBoxReadGuest.IsChecked == true)
+                checkBoxPerms[1] += 4;
+
+            if (CheckBoxWriteGuest.IsChecked == true)
+                checkBoxPerms[1] += 2;
+
+            if (CheckBoxExecuteGuest.IsChecked == true)
+                checkBoxPerms[1] += 1;
+
+            // Execute rwx
+            if (CheckBoxReadOther.IsChecked == true)
+                checkBoxPerms[2] += 4;
+
+            if (CheckBoxWriteOther.IsChecked == true)
+                checkBoxPerms[2] += 2;
+
+            if (CheckBoxExecuteOther.IsChecked == true)
+                checkBoxPerms[2] += 1;
+
+            // Make a permissions in numeric format
+            string result = "";
+            foreach (int i in checkBoxPerms)
+            {
+                result += i;
+            }
+
+            TextBoxNumericPerm.Text = result;
         }
 
         private void TextBoxNumericPerm_TextChanged(object sender, TextChangedEventArgs e)
@@ -48,6 +80,11 @@ namespace ChmodConverter
             {
                 Console.WriteLine(ex.Message);
             }
+        }
+    
+        private void CheckBoxesConvert(string[,] checkBoxPerms)
+        {
+
         }
     }
 }
